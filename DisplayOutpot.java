b8afb4652de87ae552aa9985558dc9494f4a2f93
Rinrin Karmila <rinrinkarmila26@gmail.com>
2019-10-09 05:49:39 +0700

    file display kalkulator

---
+++ DisplayOutpot.java
@@ -0,0 +1,11 @@
+public class DisplayOutpot{
+    
+
+    public void displayOut(int hasilJumlah, int bilangan1, int bilangan2){
+        System.out.println("hasil perhitungan angka " +bilangan1+ " dengan " +bilangan2+ " adalah = " +hasilJumlah);
+    }
+
+    //public String Message(){
+      //  return "error";
+    //}
+}
